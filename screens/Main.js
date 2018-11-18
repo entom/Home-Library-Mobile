d759b4aa11d8c328b1373859758a7efc7b4e0a4d
entom <tomasz@entom.info>
2018-11-18 21:52:45 +0100

    added fab element, sidemenu

--- screens/Main.js
+++ screens/Main.js
@@ -1,7 +1,8 @@
-import {Button, Container, Content, Form, Input, Item, Label, Text, Header, Left, Right, Body, Title, Icon, Toast, Root, List, ListItem} from "native-base"
+import {Button, Container, Content, Form, Input, Item, Label, Text, Header, Left, Right, Body, Title, Icon, Toast, Root, List, ListItem, Drawer, Fab} from "native-base"
 import {View} from "react-native";
 import React from "react"
 import { withNavigation } from 'react-navigation';
+import Sidebar from "./elements/Sidebar";
 
 class Main extends React.Component{
   static navigationOptions = {
@@ -10,47 +11,73 @@ class Main extends React.Component{
 
   constructor (props) {
     super(props)
+    this.state = {
+      active : false
+    }
+  }
+
+  closeDrawer = () => {
+    this.drawer._root.close();
+  }
+
+  openDrawer = () => {
+    this.drawer._root.open();
   }
 
   render() {
     return (
       <Root>
-        <Container>
-          <Header>
-            <Left>
-              <Text>Title</Text>
-            </Left>
-            <Right>
-              <Button transparent onPress={() =>
-                  Toast.show({
-                    text: 'Menu clicked',
-                    buttonText: 'OK'
-                  })
-                }>
-                  <Icon name='menu' />
+        <Drawer
+          type="displace"
+          ref={(ref) => {this.drawer = ref;}}
+          content={<Sidebar navigator={this.navigator} />}
+          onclose={() => this.closeDrawer()}>
+          <Container>
+            <Header>
+              <Left>
+                <Text>Title</Text>
+              </Left>
+              <Right>
+                <Button transparent onPress={() => {
+                  this.openDrawer()
+                }}>
+                    <Icon name='menu' />
                 </Button>
-            </Right>
-          </Header>
-          <Content>
-            <List>
-              <ListItem itemDivider>
-                <Text>A</Text>
-              </ListItem>
-              <ListItem>
-                <Text>Aaron Bennet</Text>
-              </ListItem>
-              <ListItem>
-                <Text>Ali Connors</Text>
-              </ListItem>
-              <ListItem itemDivider>
-                <Text>B</Text>
-              </ListItem>
-              <ListItem>
-                <Text>Bradley Horowitz</Text>
-              </ListItem>
-            </List>
-          </Content>
-        </Container>
+              </Right>
+            </Header>
+            <Content>
+              <List>
+                <ListItem itemDivider>
+                  <Text>A</Text>
+                </ListItem>
+                <ListItem>
+                  <Text>Aaron Bennet</Text>
+                </ListItem>
+                <ListItem>
+                  <Text>Ali Connors</Text>
+                </ListItem>
+                <ListItem itemDivider>
+                  <Text>B</Text>
+                </ListItem>
+                <ListItem>
+                  <Text>Bradley Horowitz</Text>
+                </ListItem>
+              </List>
+            </Content>
+            <Fab
+              active={this.state.active}
+              direction="up"
+              containerStyle={{ }}
+              style={{ backgroundColor: '#5067FF' }}
+              position="bottomRight"
+              onPress={() => this.setState({ active: !this.state.active })}>
+              <Icon name="options" />
+              <Button>
+                <Icon name="add" />
+              </Button>
+            </Fab>
+          </Container>
+        </Drawer>
       </Root>
     )
   }
---
+++ screens/elements/Sidebar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {Text, Container, Header, Footer, Content} from 'native-base';
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+
+        </Header>
+        <Content>
+
+        </Content>
+        <Footer>
+
+        </Footer>
+      </Container>
+    )
+  }
+}
